5843be8db1eec5530748686885ec96295ae1156c
Martins Isele <misele@opus2.com>
2016-09-07 11:25:42 +0100

    Find Chronology Entry
    -find chronology entry from list

--- runTest.bat
+++ runTest.bat
@@ -2,7 +2,7 @@
 
 ::TEST SUITE CAPBILITIES
 SET PROJECT_DIR=%CD%
-SET META_FILTER="-customTypes +ManageChronologies"
+SET META_FILTER="-customTypes +FindChronologyEntry"
 SET TEST_RESULTS=TestResults
 
 echo Removing previous test results...
